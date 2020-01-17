234ef17c71f45196aeac3a1bafe38a215176540d
oekshido <joseugarte2262005@gmail.com>
2020-01-17 09:38:24 -0800

    General imporovement and adding new functions

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+var snowball = require('node-snowball');
+
 function findSpaces(text) {
     /**
      * Takes a string as input, outputs indices of spaces in the string. Also, replaces all whitespace characters with spaces
@@ -14,8 +16,8 @@ function findSpaces(text) {
             spaceIndices.push(i);
         }
     }
-    spaceIndices.unshift(-1)
-    spaceIndices.push(text.length)
+    spaceIndices.unshift(-1);
+    spaceIndices.push(text.length);
     return spaceIndices
 };
 
@@ -28,19 +30,62 @@ function getWords(text, spaceIndices) {
      * Out: [   [ 'Hello,', 'my', 'name', 'is' ] ,
      * [[ 0, 6 ], [ 7, 9 ], [ 10, 14 ], [ 15, 17 ]]   ]
      */
-    words = []
-    indicesList = []
+    words = [];
+    indicesList = [];
     var len = spaceIndices.length - 1; //One is substracted because the for loop uses two elements
     for (var i = 0; i < len; i++) {
         if (spaceIndices[i + 1] - spaceIndices[i] > 1) {    //Doesn't count two spaces in a row
             var wordStart = spaceIndices[i] + 1, wordEnd = spaceIndices[i+1];
-            words.push(text.slice(wordStart, wordEnd))
-            indicesList.push([wordStart, wordEnd])
-        }
-    }
+            words.push(text.slice(wordStart, wordEnd));
+            indicesList.push([wordStart, wordEnd]);
+        };
+    };
     return [words, indicesList]
 };
 
-function removeStopWords(words, language = "english"){
-    
-}
+function normalizeAndremoveStopWords(words, indicesList, language = "english"){
+    /**
+     * Leaves only allowed characters on each word and lowers it, and then removes the stopwords (from stopwords.json)
+     * 
+     * In: [ 'Hello,', 'my', 'name', 'is', 'jazz' ] ,  [[ 0, 6 ], [ 7, 9 ], [ 10, 14 ], [ 15, 17 ], [20,25]]
+     * Out: [ [ 'jazz' ], [ [ 20, 25 ] ] ]
+     * (only jazz is not a stopword)
+     */
+    json = require('./stopwords.json');
+    stopwords = json[language];
+    regex = RegExp(json[language+"regex"][0], json[language+"regex"][1]); //All not allowed characters
+    newWords = [];
+    newIndicesList = [];
+    len = words.length;
+    for (i = 0; i < len; i++){
+        words[i] = words[i].toLowerCase().replace(regex, "");
+        if (!stopwords.includes(words[i])){
+            newWords.push(words[i]);
+            newIndicesList.push(indicesList[i]);
+        };
+    };
+    return [newWords, newIndicesList]
+};
+
+function shingleAndStemmer(words, indicesList, shingleSize = 1, language = "english"){
+    /**
+     * Stems the words (turns to root form) and optionally shingles them
+     * 
+     * Read more:
+     * https://en.wikipedia.org/wiki/Stemming
+     * https://en.wikipedia.org/wiki/W-shingling
+     * 
+     * In: ["like", "jazz", "my", "jazzy", "feeling"] , [[1,2], [3,4], [5,6], [7,8], [9,10]] , 2 , "english"
+     * Out: [ [  [ 'like', 'jazz' ],[ 'jazz', 'my' ],[ 'my', 'jazzi' ],[ 'jazzi', 'feel' ] ],
+     * [ [ 1, 4 ], [ 3, 6 ], [ 5, 8 ], [ 7, 10 ] ]]
+     */
+    words = snowball.stemword(words, language);
+    shingles = [];
+    shingledIndicesList = [];
+    len = words.length - shingleSize + 1;
+    for(var i = 0; i < len; i++){
+        shingles.push(words.slice(i, i+shingleSize));
+        shingledIndicesList.push([ indicesList[i][0] , indicesList[i + shingleSize - 1][1] ])
+    };
+    return [shingles, shingledIndicesList]
+};
