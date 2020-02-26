6d836deb9ce0ec030cd00d3349efe1c8115e1543
oekshido <joseugarte2262005@gmail.com>
2020-02-26 14:10:31 -0300

    Removed debug console.log

--- index.js
+++ index.js
@@ -617,6 +617,4 @@ async function autoCitation({text="", replace = false, language="english", shing
     return text + bibliography
 };
 
-match({inputText: `Sherlock Holmes (/ˈʃɜːrlɒk ˈhoʊmz/ or /-ˈhoʊlmz/) is a fictional private detective created by British author Sir Arthur Conan Doyle. Referring to himself as a "consulting detective" in the stories, Holmes is known for his proficiency with observation, deduction, forensic science, and logical reasoning that borders on the fantastic, which he employs when investigating cases for a wide variety of clients, including Scotland Yard.`}).then(console.log)
-
 module.exports = {match, autoCitation}
